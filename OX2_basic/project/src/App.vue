d5635a5ea6847efaf7567e75c72de3153c9c6fd1
Daniel Kert <danielkertdev@gmail.com>
2024-01-09 14:32:27 +0200

    commit test done mock

--- OX2_basic/project/src/App.vue
+++ OX2_basic/project/src/App.vue
@@ -1,27 +1,52 @@
 <script setup>
-import { RouterView, RouterLink } from 'vue-router';
-import WpAPIComp from './components/WpAPIComp.vue';
-import {useSwAPIStore} from './stores/SwAPIStore.js';
+import ScrollComp from './components/widgets/ScrollComp.vue';
+import {useWpAPIStore} from './stores/WpAPIStore.js'
+import { onMounted, ref } from 'vue';
 
-import { ref, onMounted } from 'vue';
-const swAPIStore = useSwAPIStore();
-const info = ref([]);
+const wpAPI = useWpAPIStore();
+const post1 = ref([]);
 
-onMounted(async() => {
-  info.value = await swAPIStore.get('info/user');
-  console.log(info.value);
-})
+onMounted(async () => {
+  post1.value = await wpAPI.get('posts', {});
+});
+
+console.log(post1.value);
 
 </script>
 
 <template>
-    <nav>
-      <RouterLink to="/">Link1</RouterLink>
-    </nav>
 
-    <RouterView />
-    <WpAPIComp/>
+
+<div class="container">
+  <br>
+<nav class="nav navbar">
+  <div class="">
+<h1>OX2 Forecast</h1>
+<h5 class="text-muted ">Möckelö</h5>
+</div>
+
+<input class="form-control" type="text" :placeholder="'Search... ' + new Date().toJSON().slice(0, 10) " aria-label="Search" style="width: 50%;">
+
+<select class="form-select" style="width: 10%;">
+  <option value="" selected>English</option>
+  <option value="">Swedish</option>
+  <option value="">Finnish</option>
+</select>
+</nav>
+
+<ScrollComp/>
+
+
+
+</div>
 
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+</style>
