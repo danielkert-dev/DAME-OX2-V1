99b9a81167906022f48ac2af2dbbec6bc7b33fc4
Daniel Kert <danielkertdev@gmail.com>
2024-01-09 09:11:45 +0200

    Mall i daniel test 1

---
+++ OX2_basic/project/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router';
+import WpAPIComp from './components/WpAPIComp.vue';
+import {useSwAPIStore} from './stores/SwAPIStore.js';
+
+import { ref, onMounted } from 'vue';
+const swAPIStore = useSwAPIStore();
+const info = ref([]);
+
+onMounted(async() => {
+  info.value = await swAPIStore.get('info/user');
+  console.log(info.value);
+})
+
+</script>
+
+<template>
+    <nav>
+      <RouterLink to="/">Link1</RouterLink>
+    </nav>
+
+    <RouterView />
+    <WpAPIComp/>
+
+</template>
+
+<style lang="scss"></style>
